2519ec954befa7984a014393163674f567203dd8
RajathRPAI <rajathpai21@gmail.com>
2024-07-13 19:59:55 +0530

    yes

--- config/database.js
+++ config/database.js
@@ -2,31 +2,3 @@
 module.exports = {
     database : 'mongodb+srv://checking:checking1234@cluster0.t1hukpp.mongodb.net/cake-shop?retryWrites=true&w=majority&appName=Cluster0'
 }
-
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://checking:checking1234@cluster0.t1hukpp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
